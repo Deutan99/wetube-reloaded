5bf26d1c1bb806e1e488d1b8410e140945c8d0da
HEO JAE HYEOK <41223338+Deutan99@users.noreply.github.com>
2023-04-15 05:10:28 +0900

    유튜브 클론코딩 #4 Router

--- src/server.js
+++ src/server.js
@@ -1,43 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
-const app  = express();
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-// const privateMiddleware = (req, res, next) => {
-//     const url = req.url;
-//     if(url === "/protected") {
-//         return res.send("<h1>Not Allowed</h1>")
-//     }
-//     console.log("Allowed, you may continue.");
-//     next();
-// };
-
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you</h1>");
-};
-
-// const handleLogin = (req, res, next) => {
-//     return res.send("Login here.");
-// };
-
-// const handleProtected = (req, res) => {
-//     return res.send("Welcome to the private lounge.");
-// }; //마지막 함수라서 next가 필요 없음.
-
-// app.use(logger);
-// app.use(privateMiddleware);
-
-app.get("/", logger, handleHome);
-// app.get("/protected", handleProtected);
-
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🎉`);
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
-
